49f43ca98e822c93bcef384c0493d5a5583e06d2
julianecunha <julianedcunha@gmail.com>
2017-10-20 07:46:43 -0200

    Exercício 10 da Lista 02

---
+++ Lista02/10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int fibMem[1000] = {0};
+
+int fib(int n)
+{
+
+	int i;
+	if (n==0) return 0;
+	if (n<=2) return 1;
+	if (fibMem[n] != 0) return fibMem[n];
+	else {
+		fibMem[n] = fib(n-1)+fib(n-2);
+		return fibMem[n];
+	}
+
+}
+
+int main() {
+
+	int n;
+	fibMem[1] = 1;
+	fibMem[2] = 1;
+
+	scanf("%d", &n);
+	printf("%d\n", fib(n));
+	return 0;
+
+}
