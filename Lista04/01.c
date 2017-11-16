e733212396e93efde35abb450abb62e15c9bdd76
julianecunha <julianedcunha@gmail.com>
2017-11-15 22:55:20 -0200

    Exercício 01 da Lista 04

---
+++ Lista04/01.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct ponto {
+	
+	float x;
+	float y;
+	float z;
+	
+} ponto;
+
+float dist_pontos(struct ponto p1, struct ponto p2)
+{
+	float dist;
+	
+	dist = sqrt(((p1.x-p2.x)*(p1.x-p2.x))+((p1.y-p2.y)*(p1.y-p2.y))+((p1.z-p2.z)*(p1.z-p2.z)));
+	
+	return dist;
+}
+
+
+int main() {
+	
+	struct ponto p1;
+	struct ponto p2;
+	float dist;
+	
+	scanf("%f %f %f", &(p1.x), &(p1.y), &(p1.z));
+	scanf("%f %f %f", &(p2.x), &(p2.y), &(p2.z));
+	
+	dist = dist_pontos(p1, p2);
+	
+	printf("%.2f\n", dist);
+	
+}
